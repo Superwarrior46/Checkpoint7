eaf00c1af100c683b726747b489a6d6d35ad909c
Superwarrior46 <juada202004@gmail.com>
2025-12-12 18:30:45 -0500

    instanciacion arreglada y script de pruebas agregado

--- Assets/Scripts/InstancingPlayerPruebas.cs
+++ Assets/Scripts/InstancingPlayerPruebas.cs
@@ -1,13 +1,25 @@
 using Photon.Pun;
 using UnityEngine;
 
-public class InstancingPlayerPruebas : MonoBehaviourPunCallbacks
+public class InstancingPlayer : MonoBehaviourPunCallbacks
 {
     [SerializeField] private PhotonView playerPrefab;
     [SerializeField] private Transform posicionSpawn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+
         PhotonNetwork.Instantiate(playerPrefab.name, posicionSpawn.position, posicionSpawn.rotation);
     }
 }
